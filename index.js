a8cef34e721fc3199f38b053b6d5610b71c1434f
TS93 <tschram93@yahoo.com>
2022-01-24 05:11:18 -0500

    destination prompt questions for inquirer

--- index.js
+++ index.js
@@ -1,4 +1,63 @@
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 const mysql = require('mysql2');
+const ListPrompt = require('inquirer/lib/prompts/list');
 
+const db = mysql.createConnection(
+	{
+		host: 'localhost',
+		user: 'root',
+		password: 'root',
+		database: 'employee_db',
+	},
+	() => console.log('Connected to db')
+);
+
+function employeeQuestions() {
+	inquirer
+		.prompt([
+			{
+				type: list,
+				name: selection,
+				choices: [
+					'View Departments',
+					'View Roles',
+					'View Employees',
+					'Add Department',
+					'Add Role',
+					'Add Employee',
+					'Update Role',
+					'Exit',
+				],
+			},
+		])
+		.then((answers) => {
+			switch (answers.selection) {
+				case 'View Departments':
+					viewDepartments();
+					break;
+
+				case 'View Roles':
+					viewRoles();
+					break;
+
+				case 'View Employees':
+					viewEmployees();
+					break;
+				case 'Add Department':
+					addDepartment();
+					break;
+				case 'Add Role':
+					addRole();
+					break;
+				case 'Add Employee':
+					addEmployee();
+					break;
+				case 'Update Role':
+					updateRole();
+					break;
+				default:
+					process.exit(0);
+			}
+		});
+}
